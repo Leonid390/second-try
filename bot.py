cccd0d29b7bed68d9b556c2eb79600fe6f32caa4
Leonid390 <leonidmashkov390@gmail.com>
2025-03-05 18:08:41 +0300

    bot.py

--- bot.py
+++ bot.py
@@ -23,4 +23,19 @@ def ban_user(message):
     else:
         bot.reply_to(message, "Эта команда должна быть использована в ответ на сообщение пользователя, которого вы хотите забанить.")
 
+
+
+@bot.message_handler(func=lambda message: True)
+def check_link(message):
+    chat_id = message.chat.id  # сохранение id чата
+    
+    if message.reply_to_message: 
+        user_id = message.reply_to_message.from_user.id
+        user_status = bot.get_chat_member(chat_id, user_id).status 
+        if "https://" in message.text:  
+            if user_status != 'administrator' or user_status != 'creator': 
+                bot.reply_to(message, "Ссылка - бан")  
+                bot.ban_chat_member(chat_id, user_id)  
+                bot.reply_to(message, f"Пользователь @{message.reply_to_message.from_user.username} был забанен.")
+
 bot.infinity_polling(none_stop=True)
